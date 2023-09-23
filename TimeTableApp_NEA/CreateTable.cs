a2f47d3f7e393e943e3a0284fe7f9dacf90ebc97
benw0903 <137078457+benw0903@users.noreply.github.com>
2023-09-23 02:09:26 +0100

    Add files via upload

---
+++ TimeTableApp_NEA/CreateTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TimeTableApp_NEA
+{
+    class CreateTable
+    {
+        public string[,] TableRowAndColumns { get; }
+
+        public CreateTable(int rows ,int columns)
+        {
+            TableRowAndColumns = new string[rows, columns];
+        }
+        
+
+        
+    }
+}
---
+++ TimeTableApp_NEA/MainTimeTable.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TimeTableApp_NEA
+{
+    class MainTimeTable
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+███╗   ███╗██╗   ██╗████████╗██╗███╗   ███╗███████╗
+████╗ ████║╚██╗ ██╔╝╚══██╔══╝██║████╗ ████║██╔════╝
+██╔████╔██║ ╚████╔╝    ██║   ██║██╔████╔██║█████╗  
+██║╚██╔╝██║  ╚██╔╝     ██║   ██║██║╚██╔╝██║██╔══╝  
+██║ ╚═╝ ██║   ██║      ██║   ██║██║ ╚═╝ ██║███████╗
+╚═╝     ╚═╝   ╚═╝      ╚═╝   ╚═╝╚═╝     ╚═╝╚══════╝
+                                                   
+
+
+Press any key to enter.");
+
+            Console.WriteLine("");
+
+            Console.ReadKey();
+            Console.Clear();
+            ViewOrCreate();
+        }
+        static void ViewOrCreate()
+        {
+            Console.WriteLine("Would you like to view or create a table today(View/Create)");
+
+            string option = Console.ReadLine();
+            option = option.ToLower();
+
+            if(option == "view")
+            {
+                ViewTable();
+            }
+            if (option == "create")
+            {
+                TableInfo();
+
+            }
+        }
+        static void TableInfo()
+        {
+            Console.WriteLine("Hello user. How many slots for activities will you want?");
+            int numRows = int.Parse(Console.ReadLine());
+            int numColumns = 7;
+            
+            CreateTable table = new CreateTable(numRows, numColumns);
+            
+        }
+
+
+
+        static void ViewTable()
+        {
+            Console.WriteLine("Enter the name of the file you would like to view.");
+            string fileName = Console.ReadLine();
+        }
+
+
+        
+
+    }
+}
